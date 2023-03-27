79531d1f85804a99dfcdbd75b3607326c6523024
Sergey Dolin <dsame@github.com>
2023-03-27 19:50:16 +0200

    diag cache

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    id := uuid.New()
+    fmt.Println(id.String())
 }
